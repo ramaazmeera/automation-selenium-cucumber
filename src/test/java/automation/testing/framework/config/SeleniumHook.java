25d58183eae6cbd5dc91d7caa1983160eff7e166
Rama Azmeera <rama.azmeera@dwp.gov.uk>
2020-03-09 11:17:09 +0000

    added tests

--- src/test/java/automation/testing/framework/config/SeleniumHook.java
+++ src/test/java/automation/testing/framework/config/SeleniumHook.java
@@ -4,14 +4,21 @@ import automation.testing.framework.selenium.WebDriverHandler;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.ScreenshotException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SeleniumHook {
+    private static final Logger LOG = LoggerFactory.getLogger(SeleniumHook.class);
 
     private final WebDriverHandler webDriverHandler;
 
@@ -29,25 +36,30 @@ public class SeleniumHook {
 
     public void afterTestRun(Scenario scenario) throws MalformedURLException {
         if (scenario.isFailed()) {
-            webDriverHandler.getDriver().manage().window().maximize();
-            attachScreenShot(scenario);
+            logTestFailure("UI", scenario.getName());
+            for (byte[] screenshot : webDriverHandler.takeFullPageScreenShot()) {
+                scenario.embed(screenshot, "image/png");
+            }
         }
-        closeDriver();
+        webDriverHandler.destroyDriver();
     }
 
-    public byte[] attachScreenShot(Scenario scenario)  {
-        byte[] bytes = new byte[0];
-        TakesScreenshot ts = (TakesScreenshot) webDriverHandler.getDriver();
-        try {
-            bytes = ts.getScreenshotAs(OutputType.BYTES);
-            scenario.embed(bytes, "image/png");
-        } catch (ScreenshotException e) {
-            e.printStackTrace();
-        }
-        return bytes;
+//    public byte[] attachScreenShot(Scenario scenario)  {
+//        byte[] bytes = new byte[0];
+//        TakesScreenshot ts = (TakesScreenshot) webDriverHandler.getDriver();
+//        try {
+//            bytes = ts.getScreenshotAs(OutputType.BYTES);
+//            scenario.embed(bytes, "image/png");
+//        } catch (ScreenshotException e) {
+//            e.printStackTrace();
+//        }
+//        return bytes;
+//    }
+
+    private void logTestFailure(String testType, String scenarioName) {
+        LOG.warn("*******************************************************");
+        LOG.warn("{} SCENARIO FAILED: {}", testType, scenarioName);
+        LOG.warn("*******************************************************");
     }
 
-    public void closeDriver() {
-        webDriverHandler.closeDriver();
-    }
 }
--- src/test/java/automation/testing/framework/selenium/WebDriverHandler.java
+++ src/test/java/automation/testing/framework/selenium/WebDriverHandler.java
@@ -1,9 +1,10 @@
 package automation.testing.framework.selenium;
 
 import automation.testing.framework.config.EnvironmentHandler;
+import automation.testing.framework.config.SeleniumHook;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,81 +12,109 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.CapabilityType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class WebDriverHandler {
+  private static final Logger LOG = LoggerFactory.getLogger(WebDriverHandler.class);
 
-    private final EnvironmentHandler env;
+  private final EnvironmentHandler env;
 
-    private WebDriver driver;
+  private WebDriver driver;
+  private static final long PAGE_LOAD_TIMEOUT = 10;
+  private static final long IMPLICIT_WAIT = 5;
 
-    public WebDriverHandler(EnvironmentHandler environmentHandler) {
-        this.env = environmentHandler;
-    }
+  public WebDriverHandler(EnvironmentHandler environmentHandler) {
+    this.env = environmentHandler;
+  }
 
-    public WebDriver getDriver() {
-        return driver;
-    }
+  public WebDriver getDriver() {
+    return driver;
+  }
 
-    public void initWebDriver() {
+  public void initWebDriver() {
 
-        String browserName = StringUtils.defaultIfBlank(env.valueOf("browser"), "chrome");
+    String browserName = StringUtils.defaultIfBlank(env.valueOf("browser"), "chrome");
 
-        System.out.println("Getting driver for " + browserName);
+    System.out.println("Getting driver for " + browserName);
 
-        WebDriver driver;
-        switch (browserName) {
-            case "firefox":
-
-                System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
-                FirefoxOptions options = new FirefoxOptions();
-                if (StringUtils.equalsIgnoreCase(env.valueOf("headless.mode"), "false")) {
-                    options.setHeadless(false);
-                }
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver(options);
-                break;
-
-            case "ie":
-                WebDriverManager.iedriver().setup();
-                InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
-                internetExplorerOptions.setCapability("EnableNativeEvents", false);
-                internetExplorerOptions.setCapability("ignoreZoomSetting", true);
-                internetExplorerOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-                internetExplorerOptions.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
-                driver = new InternetExplorerDriver(internetExplorerOptions);
-                break;
-
-
-            default:
-                driver = getChromeDriver();
-        }
-        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-        this.driver = driver;
-    }
-
-    private WebDriver getChromeDriver() {
-        ChromeOptions options = new ChromeOptions();
+    WebDriver driver;
+    switch (browserName) {
+      case "firefox":
+        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+        FirefoxOptions options = new FirefoxOptions();
         if (StringUtils.equalsIgnoreCase(env.valueOf("headless.mode"), "false")) {
-            options.setHeadless(false);
-            options.addArguments("no-sandbox");
-            options.addArguments("disable-dev-shm-usage");
+          options.setHeadless(false);
         }
-        options.setAcceptInsecureCerts(true);
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver(options);
+        break;
 
-        WebDriverManager.chromedriver().setup();
+      case "ie":
+        WebDriverManager.iedriver().setup();
+        InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+        internetExplorerOptions.setCapability("EnableNativeEvents", false);
+        internetExplorerOptions.setCapability("ignoreZoomSetting", true);
+        internetExplorerOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+        internetExplorerOptions.setCapability(InternetExplorerDriver.IE_ENSURE_CLEAN_SESSION, true);
+        driver = new InternetExplorerDriver(internetExplorerOptions);
+        break;
 
-        return new ChromeDriver(options);
+      default:
+        driver = getChromeDriver();
+    }
+    driver.manage().deleteAllCookies();
+    driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+    driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+    this.driver = driver;
+  }
 
+  private WebDriver getChromeDriver() {
+    ChromeOptions options = new ChromeOptions();
+    if (StringUtils.equalsIgnoreCase(env.valueOf("headless.mode"), "false")) {
+      options.setHeadless(false);
+      options.addArguments("no-sandbox");
+      options.addArguments("disable-dev-shm-usage");
+    }
+    options.setAcceptInsecureCerts(true);
 
+    WebDriverManager.chromedriver().setup();
+
+    return new ChromeDriver(options);
+  }
+
+  public List<byte[]> takeFullPageScreenShot() {
+    List<byte[]> screenshots = new ArrayList<byte[]>();
+
+    try {
+      JavascriptExecutor jsExec = (JavascriptExecutor) driver;
+
+      Long windowHeight = (Long) jsExec.executeScript("return window.innerHeight;");
+      Long pageHeight = (Long) jsExec.executeScript("return document.body.scrollHeight;");
+      Long currentWindowScroll = 0L;
+
+      while (currentWindowScroll <= pageHeight) {
+        jsExec.executeScript("window.scrollTo(0, " + currentWindowScroll + ");");
+        screenshots.add(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES));
+
+        currentWindowScroll = currentWindowScroll + windowHeight;
+      }
+    } catch (WebDriverException e) {
+      LOG.error("ERROR: {}", e.getMessage());
     }
 
-    public void closeDriver() {
-        driver.quit();
+    return screenshots;
+  }
+
+  public void destroyDriver() {
+    LOG.info("Quitting driver");
+    if (driver != null) {
+      driver.quit();
+      driver = null;
     }
+  }
 }
-
-
-
